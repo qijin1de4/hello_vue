027653285fb6dc28875fcb94f761ff56c6b384ef
huqijin <huqijin@css.com.cn>
2021-09-23 00:25:08 +0800

    finished CssClasses

---
+++ src/App_LifeCycleHooks.vue
@@ -0,0 +1,90 @@
+
+<template>
+<div>
+  <nav class="navbar navbar-light sticky-top mr-3">
+  <div  class=" w-100 navbar-text ml-auto d-flex justify-content-end position-relative">
+    <div class="mr-auto d-flex align-items-end flex-column bd-highlight mb-3 position-absolute" >
+      <div class="mb-2">
+        <span class="font-weight-bold bg-white"> {{ currency(cartTotal) }}</span>
+        <button
+          class="btn btn-success btn-sm ml-3"
+          id="cartDropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          @click="displayCart = !displayCart"
+        >
+        <i class="fas fa-shopping-cart mr-1"></i>
+          {{ cart.length }}
+        </button>
+      </div>
+      <div v-if="displayCart" class="list-group" aria-labelledby="cartDropdown">
+        <div v-for="(item, index) in cart" :key="index" class="list-group-item d-flex justify-content-between">
+            <div>{{item.name}}</div>
+            <div class="ml-3 font-weight-bold">{{currency(item.price)}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<section class="container">  
+  <label for="max-price" class="form-label h2">Max Price (${{max}})</label>
+  <div class="badge bg-success ml-3">results: {{filteredProducts.length}}</div>
+
+  <input v-model.number="max" type="range" class="form-range" min="0" max="130" >
+  
+  <div v-for="(item, index) in filteredProducts" :key="item.id" id="item-list" class="row align-items-center">
+    <div class="col-1 m-auto">
+      <button class="btn btn-success" @click="add2Cart(item)">+</button>
+    </div>
+    <div class="col-sm-4">
+      <img class="img-fluid d-block" :src="item.image" :alt="item.name">
+    </div>
+    <div class="col">
+     <h3 class="text-primary">{{item.name }}</h3>
+     <p class="mb-0">{{ item.description }}</p>
+     <div class="h5 float-right">
+       {{ currency(item.price) }}</div>
+    </div>
+  </div>
+</section>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      displayCart: false,
+      max: 50,
+      cart: [],
+      products: []
+    }
+  },
+  created() {
+    fetch('http://hplussport.com/api/products/order/price')
+    .then(response => response.json())
+    .then(data => {
+      this.products = data;
+    })
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter( item => (item.price < this.max));
+    },
+    cartTotal() {
+      return this.cart.reduce((inc, item) => Number(item.price)+inc, 0);
+    }
+  },
+  methods: {
+    currency(value) {
+      return `$${Number.parseFloat(value).toFixed(2)}`
+    },
+    add2Cart(product) {
+      this.cart.push(product);
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 //import App from './App.vue'
 //import App from './App_ComputedContent.vue'
-import App from './App_Events.vue'
+//import App from './App_Events.vue'
+//import App from './App_LifeCycleHooks.vue'
+//import App from './App_InlineStyle.vue'
+import App from './App_CssClasses.vue'
 
 createApp(App).mount('#app')
